ca01e5a64fa2599187cbf00b2761c61c51d1edec
webGrandpa <goga.khilo@gmail.com>
2025-03-09 23:51:35 +0400

    introduction_JS

---
+++ index.js
@@ -0,0 +1,24 @@
+// exercise N1
+
+// function GetRectangleArea(width , height){
+//     return width * height;
+// }
+
+// console.log(GetRectangleArea(10, 15));
+// console.log(GetRectangleArea(19, 27));
+
+
+// exercise N2
+
+// function CelsiusToFahrenheit(celsius){
+//         return (celsius * 5/9 ) + 32 ;
+// }
+
+// console.log(CelsiusToFahrenheit(200));
+
+// function FahrenheitToCelsius(fahrenheit){
+//         return (fahrenheit - 32) * 5/9;
+// }
+
+// console.log(FahrenheitToCelsius(214));
+
