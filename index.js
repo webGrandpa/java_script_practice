d7cf0a70de4e599c885518c4670695e39e23bb07
webGrandpa <goga.khilo@gmail.com>
2025-03-11 20:15:36 +0400

    an empty commit

--- index.js
+++ index.js
@@ -22,3 +22,120 @@
 
 // console.log(FahrenheitToCelsius(214));
 
+
+
+//      EXSERCISES
+
+// //1
+// let myName = "Goga";
+// console.log(myName);
+
+
+// //2
+// const yearOfBirth = 1994;
+// console.log(yearOfBirth);
+
+
+// //3
+
+// function greatherThan10(num){
+//     if(num < 10){
+//         console.log("less than 10");
+//     } if(num == 10){
+//         console.log("equal to 10")
+//     }else {
+//         console.log("bigger than 10");
+//     }
+// }
+
+// console.log(greatherThan10(9));
+
+
+// //4
+// let number = "10";
+// let convert = Number(number);
+
+// console.log(convert + 5);
+
+
+// Quiz
+
+//1 - b
+//2 - b
+//3 - c
+
+// QUIZ questions
+
+//1 - c
+//2 - b
+//3 - c
+//4 - b
+//5 - c
+
+// exercises
+
+// //1 
+// function manipulation(hello){
+//     hello = 
+// }
+
+// // 2
+// function prime(num){
+//     Boolean(num / num == 1)
+// }
+// console.log(num(14));
+
+// // 3 
+
+// let array = [1 , 2 , 3 , 4];
+// function(array){
+//     array(0++);
+// }
+
+// console.log(array);
+
+// // 4
+
+// let name = "Alice";
+// let age = 24;
+// let grades = [85 , 90 , 92];
+
+
+// FINAL TEST
+
+//1 - c
+//2 - a
+//3 - e
+//4 - a
+//5 - c
+
+
+
+
+
+
+
+//quiz 
+// 1 - b
+// 2 - a
+// 3 - c
+
+// exercises 
+
+// //1
+// let sentence = "Hello World";
+// let reversedSentence = sentence.split(" ").reverse().join(" ");
+// console.log(reversedSentence); 
+
+
+// // 2
+// function greaterThan10(nums) {
+//     return nums.filter(num => num > 10);
+// }
+// console.log(greaterThan10([1, 6, 20, 31]));
+
+// // 3
+
+// let numbers = [1, 2, 3];
+// let squaredNumbers = numbers.map(num => num * num);
+// console.log(squaredNumbers);
