7199a919f949379098bb18a4dad565f1f732e6c1
webGrandpa <goga.khilo@gmail.com>
2025-03-14 12:49:08 +0400

    js learning

--- index.js
+++ index.js
@@ -1,113 +1,113 @@
-exercise N1
+// exercise N1
 
-function GetRectangleArea(width , height){
-    return width * height;
-}
+// function GetRectangleArea(width , height){
+//     return width * height;
+// }
 
-console.log(GetRectangleArea(10, 15));
-console.log(GetRectangleArea(19, 27));
+// console.log(GetRectangleArea(10, 15));
+// console.log(GetRectangleArea(19, 27));
 
 
-exercise N2
+// exercise N2
 
-function CelsiusToFahrenheit(celsius){
-        return (celsius * 5/9 ) + 32 ;
-}
+// function CelsiusToFahrenheit(celsius){
+//         return (celsius * 5/9 ) + 32 ;
+// }
 
-console.log(CelsiusToFahrenheit(200));
+// console.log(CelsiusToFahrenheit(200));
 
-function FahrenheitToCelsius(fahrenheit){
-        return (fahrenheit - 32) * 5/9;
-}
+// function FahrenheitToCelsius(fahrenheit){
+//         return (fahrenheit - 32) * 5/9;
+// }
 
-console.log(FahrenheitToCelsius(214));
+// console.log(FahrenheitToCelsius(214));
 
 
 
-     EXSERCISES
+//      EXSERCISES
 
-//1
-let myName = "Goga";
-console.log(myName);
+// //1
+// let myName = "Goga";
+// console.log(myName);
 
 
-//2
-const yearOfBirth = 1994;
-console.log(yearOfBirth);
+// //2
+// const yearOfBirth = 1994;
+// console.log(yearOfBirth);
 
 
-//3
+// //3
 
-function greatherThan10(num){
-    if(num < 10){
-        console.log("less than 10");
-    } if(num == 10){
-        console.log("equal to 10")
-    }else {
-        console.log("bigger than 10");
-    }
-}
+// function greatherThan10(num){
+//     if(num < 10){
+//         console.log("less than 10");
+//     } if(num == 10){
+//         console.log("equal to 10")
+//     }else {
+//         console.log("bigger than 10");
+//     }
+// }
 
-console.log(greatherThan10(9));
+// console.log(greatherThan10(9));
 
 
-//4
-let number = "10";
-let convert = Number(number);
+// //4
+// let number = "10";
+// let convert = Number(number);
 
-console.log(convert + 5);
+// console.log(convert + 5);
 
 
-Quiz
+// Quiz
 
-1 - b
-2 - b
-3 - c
+// 1 - b
+// 2 - b
+// 3 - c
 
-QUIZ questions
+// QUIZ questions
 
-1 - c
-2 - b
-3 - c
-4 - b
-5 - c
+// 1 - c
+// 2 - b
+// 3 - c
+// 4 - b
+// 5 - c
 
-exercises
+// exercises
 
-//1 
-function manipulation(hello){
-    hello = 
-}
+// //1 
+// function manipulation(hello){
+//     hello = 
+// }
 
-// 2
-function prime(num){
-    Boolean(num / num == 1)
-}
-console.log(num(14));
+// // 2
+// function prime(num){
+//     Boolean(num / num == 1)
+// }
+// console.log(num(14));
 
-// 3 
+// // 3 
 
-let array = [1 , 2 , 3 , 4];
-function(array){
-    array(0++);
-}
+// let array = [1 , 2 , 3 , 4];
+// function(array){
+//     array(0++);
+// }
 
-console.log(array);
+// console.log(array);
 
-// 4
+// // 4
 
-let name = "Alice";
-let age = 24;
-let grades = [85 , 90 , 92];
+// let name = "Alice";
+// let age = 24;
+// let grades = [85 , 90 , 92];
 
 
-FINAL TEST
+// FINAL TEST
 
-1 - c
-2 - a
-3 - e
-4 - a
-5 - c
+// 1 - c
+// 2 - a
+// 3 - e
+// 4 - a
+// 5 - c
 
 
 
@@ -115,78 +115,78 @@ FINAL TEST
 
 
 
-quiz 
-1 - b
-2 - a
-3 - c
+// quiz 
+// 1 - b
+// 2 - a
+// 3 - c
 
-exercises 
+// exercises 
 
-//1
-let sentence = "Hello World";
-let reversedSentence = sentence.split(" ").reverse().join(" ");
-console.log(reversedSentence); 
+// //1
+// let sentence = "Hello World";
+// let reversedSentence = sentence.split(" ").reverse().join(" ");
+// console.log(reversedSentence); 
 
 
-// 2
-function greaterThan10(nums) {
-    return nums.filter(num => num > 10);
-}
-console.log(greaterThan10([1, 6, 20, 31]));
+// // 2
+// function greaterThan10(nums) {
+//     return nums.filter(num => num > 10);
+// }
+// console.log(greaterThan10([1, 6, 20, 31]));
 
-// 3
+// // 3
 
-let numbers = [1, 2, 3];
-let squaredNumbers = numbers.map(num => num * num);
-console.log(squaredNumbers);
+// let numbers = [1, 2, 3];
+// let squaredNumbers = numbers.map(num => num * num);
+// console.log(squaredNumbers);
 
 
 
-easy lvl 
-1 exercise
+// easy lvl 
+// 1 exercise
 
-function squaredNumbers(square){
-    return square * square;
-}
+// function squaredNumbers(square){
+//     return square * square;
+// }
 
-console.log(squaredNumbers(25));
+// console.log(squaredNumbers(25));
 
-// 2 exercise
+// // 2 exercise
 
-function bigNum(arr){
-    return arr.filter(num => num > 50);
-}
+// function bigNum(arr){
+//     return arr.filter(num => num > 50);
+// }
 
-console.log(bigNum([50, 100, 30, 60, 10, 1]));
+// console.log(bigNum([50, 100, 30, 60, 10, 1]));
 
-function doubleNum(a){
-    return a / (1.5 * a);
-}
+// function doubleNum(a){
+//     return a / (1.5 * a);
+// }
 
-console.log(doubleNum(9));
+// console.log(doubleNum(9));
 
-function filterNums(Nums){
-    return Nums.filter(num => num < 7);
-}
+// function filterNums(Nums){
+//     return Nums.filter(num => num < 7);
+// }
 
-console.log(filterNums([1, 2, 3, 5, 4, 6, 7, 8, 9, 10]));
+// console.log(filterNums([1, 2, 3, 5, 4, 6, 7, 8, 9, 10]));
 
-medium level
-1
+// medium level
+// 1
 
 
-let word = "bukake";
-let string = word.split("").reverse().join("");
+// let word = "bukake";
+// let string = word.split("").reverse().join("");
 
 
 
-console.log(string);
+// console.log(string);
 
-2 
+// 2 
 
-function countLetter(word, targetLetter){
-    return word.split("").filter(letter => letter === targetLetter).length;
-}
+// function countLetter(word, targetLetter){
+//     return word.split("").filter(letter => letter === targetLetter).length;
+// }
 
 
-console.log(countLetter("lanwdonoinflainldawnodianmwodnaeofbaodma;lNOCzdsklhbcouysdabhpOINd;af", "a"));
+// console.log(countLetter("lanwdonoinflainldawnodianmwodnaeofbaodma;lNOCzdsklhbcouysdabhpOINd;af", "a"));
